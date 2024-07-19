0969147d176fdb6447d17b5f747ef94dbe77cf92
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2024-07-18 19:49:54 -0700

    finished ex 16-19

---
+++ Section06/src/EvenDigitSum.java
@@ -0,0 +1,21 @@
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789)); // 20
+        System.out.println(getEvenDigitSum(252)); // 4
+        System.out.println(getEvenDigitSum(-22)); // -1
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) return -1;
+        int sum = 0;
+
+        while (number > 0) {
+            int curNum = number % 10;
+            if (curNum % 2 == 0) {
+                sum += curNum;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
+}
